13470cb205f015d1ddba417d1f6e9283cb1cca8a
Rahul <rahulgupta2019007@gmail.com>
2026-04-06 18:54:13 +0530

    Recursive BubbleSort

--- recursive_BubbleSort.cpp
+++ recursive_BubbleSort.cpp
@@ -10,7 +10,7 @@ void recursive_Bubble(int arr[], int n)
     if (n == 1)
         return;
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n - 1; i++)
     {
         if (arr[i] > arr[i + 1])
         {
