927a8f4dd3fd1d9c1b57dcc03192b399ab9ba338
Rahul <rahulgupta2019007@gmail.com>
2026-04-20 15:51:11 +0530

    Set Matrix Zeros

--- ARRAYS/setMatrixZeros.cpp
+++ ARRAYS/setMatrixZeros.cpp
@@ -5,6 +5,8 @@ using std ::cin;
 using std ::cout;
 using namespace std;
 
+// BRUTE FORCE SOLN TC = O(n*m + x(n+m))
+// x is number of zeros
 void matrixZeros1(vector<vector<int>> &arr)
 {
     vector<pair<int, int>> zeros;
@@ -37,7 +39,55 @@ void matrixZeros1(vector<vector<int>> &arr)
     }
 }
 
+// BRUTE FORCE SOLN TC = O(n3), SC = O(1)
+void markrow(vector<vector<int>> &arr, int i)
+{
+    // this loop is to travel in the column,
+    //  coz i is already given row is already given
+    for (int j = 0; j < arr[0].size(); j++)
+    {
+        if (arr[i][j] != 0)
+            arr[i][j] = -1;
+    }
+}
+void markcolm(vector<vector<int>> &arr, int j)
+{
+    // this loop is to travel in the row,
+    //  coz j is already given column is already given
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i][j] != 0)
+            arr[i][j] = -1;
+    }
+}
 void matrixZeros2(vector<vector<int>> &arr)
+{
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for (int j = 0; j < arr[0].size(); j++)
+        {
+            if (arr[i][j] == 0)
+            {
+                markrow(arr, i);
+                markcolm(arr, j);
+            }
+        }
+    }
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for (int j = 0; j < arr[0].size(); j++)
+        {
+            if (arr[i][j] == -1)
+            {
+                arr[i][j] = 0;
+            }
+        }
+    }
+}
+
+// BETTER SOLN TC = O(2*n*m)
+//  SC = O(n+m) --> for storing indexs
+void matrixZeros3(vector<vector<int>> &arr)
 {
     vector<int> n(arr.size());
     vector<int> m(arr[0].size());
@@ -66,10 +116,10 @@ void matrixZeros2(vector<vector<int>> &arr)
     }
 }
 
-void matrixZeros3(vector<vector<int>> &arr)
+// Optimal soln TC = O(2*n*m), SC = O(1)
+void matrixZeros4(vector<vector<int>> &arr)
 {
-    int k = 0;
-    int l = 0;
+    int colm = 1;
     for (int i = 0; i < arr.size(); i++)
     {
         for (int j = 0; j < arr[i].size(); j++)
@@ -77,23 +127,44 @@ void matrixZeros3(vector<vector<int>> &arr)
 
             if (arr[i][j] == 0)
             {
-                while (k < arr.size())
+                arr[i][0] = 0;
+                if (j != 0)
                 {
-                    arr[i][k] = 0;
-                    k++;
+                    arr[0][j] = 0;
                 }
-                k = 0;
-
-                while (l < arr[0].size())
+                else
                 {
-                    arr[l][j] = 0;
-                    l++;
+                    colm = 0;
                 }
-                l = 0;
             }
         }
     }
+
+    for (int i = 1; i < arr.size(); i++)
+    {
+        for (int j = 1; j < arr[i].size(); j++)
+        {
+            if (arr[i][j] != 0)
+            {
+                if (arr[0][j] == 0 || arr[i][0] == 0)
+                {
+                    arr[i][j] = 0;
+                }
+            }
+        }
+    }
+    if (arr[0][0] == 0)
+    {
+        for (int j = 0; j < arr[0].size(); j++)
+            arr[0][j] = 0;
+    }
+    if (colm == 0)
+    {
+        for (int i = 0; i < arr.size(); i++)
+            arr[i][0] = 0;
+    }
 }
+
 int main()
 {
     int n;
@@ -108,9 +179,12 @@ int main()
             cin >> arr[i][j];
         }
     }
+
     // matrixZeros1(arr);      //brute force soln
     // matrixZeros2(arr);      //better soln
-    matrixZeros3(arr);
+    // matrixZeros3(arr);
+    matrixZeros4(arr);
+
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
