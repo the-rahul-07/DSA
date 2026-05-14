df685d26d53bfb49e755e1dd4bd5f22d89b9db1f
Rahul <rahulgupta2019007@gmail.com>
2026-05-14 19:38:00 +0530

    Kth missing no

--- BINARY SEARCH/BS on Answers/kthMissingNo.cpp
+++ BINARY SEARCH/BS on Answers/kthMissingNo.cpp
@@ -4,7 +4,27 @@
 using std ::cin;
 using std ::cout;
 using namespace std;
-int findKthPositive(vector<int> &arr, int k)
+
+// tc = O(N)
+int findKthPositive1(vector<int> &arr, int k)
+{
+    int n = arr.size();
+    int val;
+    for (int i = 0; i < n; i++)
+    {
+        val = (arr[i] - (i + 1));
+
+        if (val >= k)
+        {
+            // missing number+existing number;
+            return k + i;
+        }
+    }
+    return k + n;
+}
+
+// TC = O(logn)
+int findKthPositive2(vector<int> &arr, int k)
 {
     int n = arr.size();
     int low = 0;
@@ -33,7 +53,7 @@ int main()
     int n;
     cin >> n;
 
-    vector<int> arr;
+    vector<int> arr(n);
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
@@ -42,6 +62,7 @@ int main()
     int k;
     cin >> k;
 
-    findKthPositive(arr, k);
+    // cout<<findKthPositive1(arr, k);    //brute using linear search (n)
+    cout << findKthPositive2(arr, k); // optimal using bs logn
     return 0;
 }
